0a8d121611d7d094f697d157af712ff730fcf3d5
Administrator <sunkejl@live.com>
2014-06-26 23:59:38 +0800

    config配置，随便放在哪个模组下

---
+++ config/module.config.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 14-6-26
+ * Time: 下午11:44
+ */
+
+return array(
+
+    'assetManager'=>[
+
+        'merge'=>[
+
+            'pro/ad/global.js'=>[
+
+                'vendor/bootstrap3/js/bootstrap.min.js',
+                'vendor/jquery/jquery-1.11.1.min.js'
+            ]
+        ]
+    ]
+);
